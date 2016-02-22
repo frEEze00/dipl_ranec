7b0904a34e0c015027d5d61dc326da886be0838f
frEEze frEEze <freeze147@yandex.ru>
2016-02-22 18:36:54 +0300

    input data to HTML

--- dipl_ranec/Helper.cs
+++ dipl_ranec/Helper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace dipl_ranec {
-    class Helper {
+    class Helper : BackPack {
          DateTime timeStart;
          DateTime timeFinish;
 
@@ -17,12 +19,52 @@ namespace dipl_ranec {
             timeFinish = DateTime.Now;
         }
 
-        public double CalculateTime() {
-            double result;
-            MessageBox.Show(timeStart.TimeOfDay.ToString() + "\n" + timeFinish.TimeOfDay.ToString() + 
-                "\n" + (timeFinish.TimeOfDay - timeStart.TimeOfDay).ToString());
+        public string CalculateTime() {
+            var result = (timeFinish.TimeOfDay - timeStart.TimeOfDay).ToString();
+            MessageBox.Show(result);
+            return result;
+        }
 
-            return 0;
+        public void CreateHtmlFile(List<Item> inputItems, List<Item> outputItems) {
+            string tempFileName = "list.html";
+            using (TextWriter writer = new StreamWriter(tempFileName)) {
+                try {
+                    writer.WriteLine("<html>");
+                    writer.WriteLine("<head><title>Links</title></head>");
+                    writer.WriteLine("<body>");
+
+                    writer.WriteLine("<p>");
+
+                    writer.WriteLine("<table border=\"1\">");
+                    writer.WriteLine("<caption>Input DATA Table</caption>");
+                    writer.WriteLine("<tr><th>Cost</th><th>Mas</th><th>Use</th></tr>");
+                    foreach (var item in inputItems) {
+                        if (item.Use) {
+                            writer.WriteLine("<tr> <td>{0}</td> <td>{1}</td> <td><font color=\"green\">{2}</font></td></tr>", 
+                                item.Cost, item.Mas, item.Use);
+                        }
+                        else {
+                            writer.WriteLine("<tr> <td>{0}</td> <td>{1}</td> <td><font color=\"red\">{2}</font></td></tr>",
+                                item.Cost, item.Mas, item.Use);
+                        }
+                        
+                    }
+                    writer.WriteLine("</table>");
+                    writer.WriteLine("</p>");
+
+                    writer.WriteLine("</body>");
+                    writer.WriteLine("</html>");
+                }
+                catch (Exception ex) {
+                    System.Diagnostics.Trace.TraceError(ex.Message);
+                }
+                finally {
+                    writer.Close();
+                }
+            }
+            if (File.Exists(tempFileName)) {
+                System.Diagnostics.Process.Start(tempFileName);
+            }
         }
     }
 }
